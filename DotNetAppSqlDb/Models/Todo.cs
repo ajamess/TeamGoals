5b526a480bbb115dd689c7180a7ad23cf590e1a7
Adam Steenwyk <asteen@microsoft.com>
2018-10-06 09:25:59 -0700

    Add project files.

---
+++ DotNetAppSqlDb/Models/Todo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DotNetAppSqlDb.Models
+{
+    public class Todo
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public string HighlightsandLearnings { get; set; }
+        public string LowlightsandRequests { get; set; }
+        public string GeneralNotes { get; set; }
+
+        [Display(Name = "Created Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime CreatedDate { get; set; }
+    }
+}
